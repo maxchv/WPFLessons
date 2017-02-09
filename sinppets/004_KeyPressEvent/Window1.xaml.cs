29ff02500716e03408fd5d8151c570f109fe733a
Maxim <maxshaptala@gmail.com>
2017-02-10 00:51:10 +0200

    добавлены примеры работы с маршрутизируемыми событиями

---
+++ sinppets/004_KeyPressEvent/Window1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _005_KeyPressEvent
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            События в порядке их возникновения:
+            1. PreviewKeyDown – нажатие клавиши.
+            2. KeyDown – нажатие клавиши.
+            3. PreviewTextInput – нажатие завершено и элемент получил текстовый ввод. (не работает для тех клавиш, которые не отображаются)
+            4. TextChanged – смена текста в элементе управления.
+            5. PreviewKeyUp – отпускание клавиши.
+            6. KeyUp - отпускание клавиши.
+            Все события Preview нужны для того, что бы отловить события, которые были перехвачены стандартной реализацией элемента управления. 
+            Например, TextBox блокирует клавиши управление курсором, так как обрабатывает их самостоятельно.
+         */
+
+        private void KeyEvent(object sender, KeyEventArgs e)
+        {
+            string message = 
+                "Event: " + e.RoutedEvent + " " +
+                " Key: " + e.Key;
+            lstMessages.Items.Add(message);
+        }
+
+        private void TextInputHandler(object sender, TextCompositionEventArgs e)
+        {
+            string message = 
+                "Event: " + e.RoutedEvent + " " +
+                " Text: " + e.Text;
+            lstMessages.Items.Add(message);
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string message =
+                "Event: " + e.RoutedEvent;
+            lstMessages.Items.Add(message);
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e)
+        {
+            lstMessages.Items.Clear();
+        }
+    }
+}
---
+++ sinppets/RoutedEvents/RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvents
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"Button Name: {(sender as Button).Name}");
+            builder.AppendLine($"OriginalSource: {e.OriginalSource}");
+            builder.AppendLine($"Source: {e.Source}");
+            builder.AppendLine($"Handled: {e.Handled}");
+            MessageBox.Show(builder.ToString());
+        }
+
+        private void Window_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine($"Button Name: {sender.ToString()}");
+            builder.AppendLine($"OriginalSource: {e.OriginalSource}");
+            builder.AppendLine($"Source: {e.Source}");
+            builder.AppendLine($"Handled: {e.Handled}");
+            MessageBox.Show(builder.ToString());
+        }
+    }
+
+    class ExtraButton: Button
+    {
+        #region ExtraClick Routed Event
+
+        public static readonly RoutedEvent ExtraClickEvent = EventManager.RegisterRoutedEvent(
+            "ExtraClick",
+            RoutingStrategy.Direct,
+            typeof(RoutedEventHandler),
+            typeof(ExtraButton));
+
+        public event RoutedEventHandler ExtraClick
+        {
+            add { AddHandler(ExtraClickEvent, value); }
+            remove { RemoveHandler(ExtraClickEvent, value); }
+        }
+
+        /// <summary>
+        /// Invoke this method when you wish to raise a(n) ExtraClick event
+        /// </summary>
+        protected override void OnClick()
+        {
+            base.OnClick();
+            RoutedEventArgs newEventArgs = new RoutedEventArgs(ExtraButton.ExtraClickEvent);
+            RaiseEvent(newEventArgs);
+        }
+
+        #endregion
+    }
+}
