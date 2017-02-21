131c4780f8e49b3b6088fa61fe253a52eb386f88
Maxim <maxshaptala@gmail.com>
2017-02-21 12:24:13 +0200

    добавлены примеры анимации и материалы занятия

---
+++ Workspace/WpfLesson14/WpfAnimationDemo/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAnimationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ColorAnimation a = new ColorAnimation();
+            a.To = Colors.Yellow;
+            a.Duration = new Duration(TimeSpan.Parse("0:0:3"));
+            tb.Background.BeginAnimation(SolidColorBrush.ColorProperty, a);
+        }
+
+        private void b_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation a = new DoubleAnimation();
+            //a.From = 150;
+            //a.To = 150;
+            a.By = 100;
+            a.AutoReverse = true;
+            a.SpeedRatio = 10;
+            a.RepeatBehavior = new RepeatBehavior(TimeSpan.Parse("0:0:1"));
+            a.BeginTime = TimeSpan.Parse("0:0:1");
+            // 1 с - Duration.Automatic;
+            a.Duration = new Duration(TimeSpan.Parse("0:0:5"));
+            //a.FillBehavior = FillBehavior.Stop;
+            a.Completed += A_Completed;
+            a.AccelerationRatio = 0.5;
+            a.DecelerationRatio = 0.5;
+            b.BeginAnimation(Button.WidthProperty, a);
+            //BeginAnimation(Window.HeightProperty, a);
+        }
+
+        private void A_Completed(object sender, EventArgs e)
+        {
+            Title = "Completed";
+        }
+    }
+}
---
+++ Workspace/WpfLesson14/WpfLesson14/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfLesson14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        int count;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            tb.Text = (count++).ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!timer.IsEnabled)
+            {                
+                timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            b.Width += 1;
+            if(b.Width >= 150)
+            {
+                timer.Stop();
+            }
+        }
+    }
+}
