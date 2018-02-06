d657e8e8d39bb7017a280368f84dc9d79b1abb26
Maxim Shaptala <maxshaptala@gmail.com>
2018-02-06 17:52:15 +0200

    added homeworks

--- Workspace/WpfLesson03/EditXaml/MainWindow.xaml.cs
+++ Workspace/WpfLesson03/EditXaml/MainWindow.xaml.cs
@@ -26,11 +26,8 @@ namespace EditXaml
     {
         string xaml = @"<Window 
         xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'
-        xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
-        xmlns:d='http://schemas.microsoft.com/expression/blend/2008'
-        xmlns:mc='http://schemas.openxmlformats.org/markup-compatibility/2006'
-        xmlns:local='clr-namespace:EditXaml'
-        mc:Ignorable='d'
+        xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'        
+        xmlns:mc='http://schemas.openxmlformats.org/markup-compatibility/2006'       
         Title='MainWindow' Height='350' Width='525'>
     <Grid>
         <Grid.RowDefinitions>
